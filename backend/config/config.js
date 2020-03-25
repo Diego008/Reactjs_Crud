681971da1db2a3efa67cb82bb8149d533f8b804d
Diego008 <diegoaugusto008@gmail.com>
2020-03-25 11:57:46 -0300

    Iniciando backend

---
+++ backend/config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  username: 'root',
+  password: 'root',
+  database: 'crudreactjs',
+  host: '127.0.0.1',
+  dialect: 'mysql',
+  port: '3306',
+  define: {
+    timestamps: true
+  }
+}
