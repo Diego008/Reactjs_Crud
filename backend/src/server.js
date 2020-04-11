87aeab99a2815bde94d3f871d30055d4c1667f54
Diego008 <diegoaugusto008@gmail.com>
2020-04-11 12:34:43 -0300

    atualização

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const routes = require('./routes');
+const cors = require('cors');
+const port = process.env.PORT || 3333;
 const database = require('../database/index');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(routes);
-app.listen(3333);
+app.listen(port, () => {
+    console.log(`Projeto executando na porta: ${port}`)
+});
